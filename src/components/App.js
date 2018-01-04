86aa3c9d4aef6b7da8c98621486056d2d192e0d1
chengy1 <15294692443@163.com>
2018-01-04 11:18:32 +0800

    build

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ImgFigure from './ImgFigure.jsx';
 import '../css/App.css';
 import data from '../data/imageDatas.json';
 
@@ -8,14 +9,39 @@ let imageData = (function getImageUrl(dataArr) {
   });
   return dataArr
 })(data);
-console.log(imageData)
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.Constant = {
+      centerPos: {  // 中心点
+      left: 0,
+      right: 0
+    },
+    hposRange: {  // 水平方向的取值范围
+      leftSecX: [0, 0],
+      rightSecX: [0, 0],
+      y: [0, 0]
+    },
+    vPosRange: {  // 垂直方向的取值范围
+      x: [0, 0],
+      topY: [0, 0]
+    }
+  }
+  }
   render() {
+    let controllerUnits = [];
+    let ImgFigures = [];
+    imageData.forEach((item) => {
+      ImgFigures.push(<ImgFigure data={item} key={item.fileName}/>)
+    })
     return (
       <div className="stage">
         <section className="image-sec">
+          {ImgFigures}
         </section> 
         <nav className='controller-nav'>
+          {controllerUnits}
         </nav>
       </div>
     );
